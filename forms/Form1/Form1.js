b936191fb8bb00ca2de00ed3bdb3f2b751e63c95
ryanlobes13 <90591769+ryanlobes13@users.noreply.github.com>
2021-09-22 16:16:17 -0500

    finished while loop

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,11 @@
+
+let num = prompt(`Enter the number of iterations you want!`)
+let i = 1
+
+while (i < num){
+  console.log(`This is loop iteration ${i}`)
+  
+  i = i + 1}
+  
+  
+console.log(`This is loop iteration ${i}`)
