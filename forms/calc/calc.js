6dd72c71c7d9f84706d904be1d8c121423e19c69
ryanlobes13 <90591769+ryanlobes13@users.noreply.github.com>
2021-09-22 16:56:59 -0500

    working on it

---
+++ forms/calc/calc.js
@@ -0,0 +1,6 @@
+/*let num1 = prompt(`Enter first number`)
+let num2 = propmt(`Enter second number`)
+let calculate = sum(num1,num2)
+
+console.log(The sum of ${num2} and ${num1} is ${calculate}) 
+*/
---
+++ forms/strings/strings.js
@@ -0,0 +1,3 @@
+let num = prompt(`Number added ny 10`)
+cool = num + 10
+console.log(${cool})
